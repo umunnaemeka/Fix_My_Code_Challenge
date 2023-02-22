828eea20df766cb19a74f3fc12d9a61bc3210c13
umunnaemeka <umunnaemeka@gmail.com>
2023-02-22 08:54:15 -0800

    fizbuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -16,11 +16,11 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if i % 3 == 0:
-            tmp_result.append("Fizz")
-        elif i % 3 == 0 and i % 5 == 0:
+        if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        elif i % 5 == 0:
+        elif (i % 3) == 0:
+            tmp_result.append("fizz")
+        elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
@@ -36,4 +36,3 @@ if __name__ == '__main__':
 
     number = int(sys.argv[1])
     fizzbuzz(number)
-
